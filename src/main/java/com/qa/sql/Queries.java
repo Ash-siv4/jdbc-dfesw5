f3157477dfa3662c1630748dc832ca00cdc7234e
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2021-11-19 15:44:27 +0000

    AS: delete method completed

--- src/main/java/com/qa/sql/Queries.java
+++ src/main/java/com/qa/sql/Queries.java
@@ -17,14 +17,11 @@ public class Queries {
 	// Connect to database, we need: url, username, password
 	// static - only one instance of it
 	// final - fixed
-//	public static final String URL = "jdbc:mysql://localhost:3306/animals";
-//	public static final String USER = "root";
-//	public static final String PASS = "root";
 
 	private Connection conn;
 	private Statement stmt;
 
-	// open connection in the constructor - intialise anything
+	// open connection in the constructor - initialise anything
 	public Queries() {
 		// Exceptions -> Abnormal conditions which affect the flow of the application
 		// but can be handled using try-catch blocks
@@ -46,7 +43,8 @@ public class Queries {
 
 	// CREATE - INSERT INTO .... -> returns nothing, just say "OK"
 	public void create(String mName, String col, String orig) {
-		String createStmt = "INSERT INTO mammals(mammal_name, colour, origin) VALUES('" + mName + "','" + col + "','"+ orig + "');";
+		String createStmt = "INSERT INTO mammals(mammal_name, colour, origin) VALUES('" + mName + "','" + col + "','"
+				+ orig + "');";
 		try {
 			stmt.executeUpdate(createStmt);
 			System.out.println("Statement executed");
@@ -55,26 +53,48 @@ public class Queries {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// READ - SELECT .... -> returns info
-		public void read() {
-			String read = "SELECT * FROM mammals;";
-			ResultSet rs;
-			
-			try {
-				rs = stmt.executeQuery(read);
-				while(rs.next()) {
-					System.out.println("ID:" + rs.getInt("id") + ", Mammal Name: "+ rs.getString("mammal_name")+ ", Colour: "+ rs.getString("colour"));
-				}
-				
-				
-			} catch (SQLException e) {
-				System.out.println("Bad query");
-				e.printStackTrace();
+	public void read() {
+		String readStmt = "SELECT * FROM mammals;";
+		ResultSet rs;
+		try {
+			rs = stmt.executeQuery(readStmt);
+			while (rs.next()) {
+				System.out.println("ID:" + rs.getInt("id") + "| MammalName: " + rs.getString("mammal_name")
+						+ "| Colour: " + rs.getString("colour") + "| Origin: " + rs.getString("origin"));
 			}
-			
+		} catch (SQLException e) {
+			System.out.println("Bad query");
+			e.printStackTrace();
 		}
-	
-	
+	}
+
+	// UPDATE - UPDATE .... -> returns nothing, just say "OK"
+	public void update() {
+
+	}
+
+	// DELETE - DELETE .... -> returns nothing, just say "OK"
+	public void delete(int id) {
+		String delStmt = "DELETE FROM mammals WHERE id=" + id + ";";
+		try {
+			stmt.executeUpdate(delStmt);
+			System.out.println("Statement executed");
+		} catch (SQLException e) {
+			System.out.println("Bad query");
+			e.printStackTrace();
+		}
+	}
+
+	// close the connection
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("Closing connection...");
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/main/java/com/qa/sql/Runner.java
+++ src/main/java/com/qa/sql/Runner.java
@@ -30,47 +30,57 @@ public class Runner {
 //			System.out.println(arr[2]);
 //		}
 
-		//creating an instance of the Queries class and calling the constructor
+		// creating an instance of the Queries class and calling the constructor
 		Queries q = new Queries();
-		
-		// Ask for user input on CRUD choice and store it in the getInput() method, and now store it in the crud variable
+
+		// Ask for user input on CRUD choice and store it in the getInput() method, and
+		// now store it in the crud variable
 		String crud = getInput();
 
-		//do-while loop to start the loop
-		do {
-			//switch-case to match which CRUD to perform
-			switch (crud.toLowerCase()) {
-			case "create":
-				System.out.println("Enter mammal name: ");
-				String n = sc.nextLine();
-				System.out.println("Enter colour: ");
-				String c = sc.nextLine();
-				System.out.println("Enter origin: ");
-				String o = sc.nextLine();
-				q.create(n, c, o);
-				break;
-			case "read":
-				q.read();
-				break;
-			case "update":
-
-				break;
-			case "delete":
-
-				break;
-			default:
-				System.out.println("Invalid CRUD choice");
-			}
-			//Check if the user wants to continue or break out of the loop
-			System.out.println("Would you like to continue (y/n)? ");
-			String quit = sc.nextLine();
-			if(quit.toLowerCase().equals("y")) {
-				crud = getInput();
-			}else {
-				crud = "exit";
-			}
-		} while (!crud.equals("exit"));
-		System.out.println("BYEEE");
-
+		// try-finally -> try some code and once it has executed, carry out what the
+		// finally block says to do (ideal for closing a connection once the user no
+		// longer wants to continue with the application
+		try {
+			// do-while loop to start the loop
+			do {
+				// switch-case to match which CRUD to perform
+				switch (crud.toLowerCase()) {
+				case "create":
+					System.out.println("Enter mammal name: ");
+					String n = sc.nextLine();
+					System.out.println("Enter colour: ");
+					String c = sc.nextLine();
+					System.out.println("Enter origin: ");
+					String o = sc.nextLine();
+					q.create(n, c, o);
+					break;
+				case "read":
+					q.read();
+					break;
+				case "update":
+					q.update();
+					break;
+				case "delete":
+					System.out.println("Enter id of the mammal you would like to delete: ");
+					int delId = sc.nextInt();
+					sc.nextLine();// to catch the enter key
+					q.delete(delId);
+					break;
+				default:
+					System.out.println("Invalid CRUD choice");
+				}
+				// Check if the user wants to continue or break out of the loop
+				System.out.println("Would you like to continue (y/n)? ");
+				String quit = sc.nextLine();
+				if (quit.toLowerCase().equals("y")) {
+					crud = getInput();
+				} else {
+					crud = "exit";
+				}
+			} while (!crud.equals("exit"));
+			System.out.println("BYEEE");
+		} finally {
+			q.close();
+		}
 	}
 }
